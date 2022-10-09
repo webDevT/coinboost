124cf87e4db20b90de0f87f27a8882c7c7013461
karinadesyatnik <karinadesyatnik27@gmail.com>
2022-10-09 16:53:52 +0300

    home page

---
+++ docs/js/main.js
@@ -0,0 +1,123 @@
+$(function(){
+
+
+   // ------start sticky header------
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1){
+    $('.header').addClass("hide");
+    }
+    else{
+    $('.header').removeClass("hide");
+    }
+    });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 10){
+        $('.pricebar').slideDown()
+        }
+        else{
+       $('.pricebar').slideUp();
+        }
+        });   
+    //-------start mobil menu---------- 
+//start updates
+var lastScrollTop = 0;
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if (st > lastScrollTop || $(this).scrollTop() < 1) {
+
+       $('.header').removeClass('sticky');
+       
+   } else {
+
+      $('.header').addClass('sticky');
+   }
+   lastScrollTop = st;
+});
+
+//end updates
+
+
+// start width control on hover menu link 
+if($(window).width() > 768) {
+$('.header__menu-link').each(function(){
+    $(this).width($(this).width() + 4);
+});
+}
+// end width control on hover menu link 
+
+//start mobile menu 
+$('.menu__btn').click(function(){
+    $(this).toggleClass('active');
+    $('.menu').slideToggle();
+})
+//end mobile menu 
+
+//start mobile sub-menu 
+if($(window).width() < 768) {
+ $('.header__menu-item--sub').click(function(){
+    $('.sub__menu').slideToggle();
+ })
+ $('.footer__menu-link--sub').click(function(){
+    $('.footer__menu-sub').slideToggle();
+ })
+}
+//end mobile menu sub-menu
+
+//start slider init
+$('.slider').slick({
+    prevArrow: false,
+    nextArrow: false,
+    fade: true,
+    autoplay: true
+});
+
+$('.about-slider').slick({
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    slidesToShow: 3,
+    dots: true,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+             
+            }
+          },
+    ]
+
+});
+
+//end slider init
+
+$('.slider').on('afterChange', function(event, slick, currentSlide){
+    if (currentSlide == 0) {
+        $('.header').addClass('light');
+        $('.header').removeClass('orange');
+        $('.header').removeClass('grey');
+     }
+    
+    if (currentSlide == 1) {
+        $('.header').addClass('orange');
+        $('.header').removeClass('grey');
+        $('.header').removeClass('light');
+     }
+     if (currentSlide == 2) {
+        $('.header').addClass('grey');
+        $('.header').removeClass('orange');
+        $('.header').removeClass('light');
+     }
+   
+});
+
+
+});
+
